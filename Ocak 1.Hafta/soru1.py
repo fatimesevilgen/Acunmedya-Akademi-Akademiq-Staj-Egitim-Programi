9c587f6239361be4e47e1430f0ff5199df2ac3cc
fatimesevilgen <fatimesevilgen@gmail.com>
2025-03-20 17:17:19 +0300

    ocak hafta 1

---
+++ Ocak 1.Hafta/soru1.py
@@ -0,0 +1,5 @@
+name = input("Adınız : ")
+age = int(input("Yaşınız : "))
+year = int(input("Doğum yılınız : "))
+
+print(f"Merhaba {name}! {age} yaşındasın ve {year} yılında doğmuşsun.")
---
+++ Ocak 1.Hafta/soru2.py
@@ -0,0 +1,7 @@
+num1 = int(input("Birinci sayıyı giriniz : "))
+num2 = int(input("İkinci sayıyı giriniz : "))
+
+print(f"Toplam : {num1 + num2}")
+print(f"Fark : {num1 - num2}")
+print(f"Çarpım : {num1 * num2}")
+print(f"Bölüm : {num1 / num2}")
